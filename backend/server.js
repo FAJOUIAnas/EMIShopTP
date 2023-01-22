c94ddd81e18655600d1fe63272eb377dc6a83ac7
Ayman EL KIHEL <91762035+Jinkudo200@users.noreply.github.com>
2023-01-22 19:00:30 +0100

    final 2

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const path = require('path');
-const PORT = process.env.PORT || 5000; // si le port est précisé sinon 5000
+const PORT = process.env.PORT || 80; // si le port est précisé sinon 5000
 const HOST = process.env.Host || 'localhost';
 
 const app = express()
